63dc96998fa28f82ddd282fc4c79f438a5a0bb54
jamesnazzelofficial <jamesnazzeldevelopment@gmail.com>
2024-06-27 17:30:19 +0800

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import webhookRoutes from './routes/webhookRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(helmet()); // Adds security headers
